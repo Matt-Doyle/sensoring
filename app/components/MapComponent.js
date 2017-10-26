02fb0f2b13894a5924dc6d2a75301fa6f92166f1
Matthew Doyle <doy0001@jmss.vic.edu.au>
2017-10-26 23:09:18 +1100

    More work on the maps

--- app/components/MapComponent.js
+++ app/components/MapComponent.js
@@ -5,39 +5,37 @@ class MapComponent extends React.Component {
     render() {
         var floorMap;
         var level = this.props.level;
-        console.log(level);
         switch (level) {
             case 1:
             case 2:
-                console.log(level);
                 floorMap = (
                     <div className="mapLayout">
                         <div className="mapUpper">
-                            <div>1L1 &amp; 2</div>
-                            <div>1B1 &amp; 2</div>
-                            <div>1L3</div>
-                            <div>1L2 &amp; 4</div>
-                            <div>1B8 &amp; 9</div>
+                            <div>{level}L1 &amp; 2</div>
+                            <div>{level}B1 &amp; 2</div>
+                            <div>{level}L3</div>
+                            <div>{level}L2 &amp; 4</div>
+                            <div>{level}B8 &amp; 9</div>
                         </div>
 
                         <div className="mapCenter">
                             <div>Stair Area</div>
-                            <div>THE VOID</div>
+                            <div className="mapEmptyArea">THE VOID</div>
                             <div>Stair Area</div>
                         </div>
 
                         <div className="mapLower">
-                            <div>1B1 &amp; 2</div>
-                            <div>1L1 &amp; 2</div>
-                            <div>1L3</div>
-                            <div>1L2 &amp; 4</div>
-                            <div>1B8 &amp; 9</div>
+                            <div>{level}B1 &amp; 2</div>
+                            <div>{level}L1 &amp; 2</div>
+                            <div>{level}L3</div>
+                            <div>{level}L2 &amp; 4</div>
+                            <div>{level}B8 &amp; 9</div>
                         </div>
 
                     </div>
                 );
                 break;
-            case "g":
+            case 0:
                 floorMap = (<div className='mapFloor'></div>);
                 break;
             default:
--- app/components/Pages/Maps.js
+++ app/components/Pages/Maps.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import MapComponent from '../MapComponent';
 
+var fullHeight = {
+  height: '100%'
+};
+
 class Maps extends React.Component {
   constructor() {
     super();
@@ -8,8 +12,28 @@ class Maps extends React.Component {
 
   render() {
     return (
-      <div>
-        <MapComponent level={1}/>
+      <div className="container-fluid">
+        <div className="row row-eq-height">
+          <div className="col-md-7">
+            <div className="panel" style={fullHeight}>
+              <div className="panel-body">
+                <h3>Map of Level G</h3>
+                <MapComponent level={0}/>
+                <h3>Map of Level 1</h3>
+                <MapComponent level={1}/>
+                <h3>Map of Level 2</h3>
+                <MapComponent level={2}/>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div className="panel" style={fullHeight}>
+              <div className="panel-body">
+                <h3>Real-time Graphs</h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
