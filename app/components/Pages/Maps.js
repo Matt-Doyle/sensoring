0d331be6f64f95bf3e0ada0131e6fb996c55dc05
Matt-Doyle <doy0001@jmss.vic.edu.au>
2017-10-30 14:20:45 +1100

    help me

--- app/components/Pages/Maps.js
+++ app/components/Pages/Maps.js
@@ -5,6 +5,11 @@ var fullHeight = {
   height: '100%'
 };
 
+var padding = {
+  marginTop: '2em'
+};
+
+
 class Maps extends React.Component {
   constructor() {
     super();
@@ -13,21 +18,22 @@ class Maps extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <div className="row row-eq-height">
-          <div className="col-md-7">
-            <div className="panel" style={fullHeight}>
+        <div className="row">
+          <div className="col-sm-6">
+            <div className="panel">
               <div className="panel-body">
-                <h3>Map of Level G</h3>
+                <h3>Maps</h3>
+                <h4 style={padding}>G</h4>
                 <MapComponent level={0}/>
-                <h3>Map of Level 1</h3>
+                <h4 style={padding}>1</h4>
                 <MapComponent level={1}/>
-                <h3>Map of Level 2</h3>
+                <h4 style={padding}>2</h4>
                 <MapComponent level={2}/>
               </div>
             </div>
           </div>
-          <div className="col-md-5">
-            <div className="panel" style={fullHeight}>
+          <div className="col-sm-6">
+            <div className="panel">
               <div className="panel-body">
                 <h3>Real-time Graphs</h3>
               </div>
